a1eb17380525f52d2926dd42ff09f064e418935d
Kijanczuk, Damian <s20154@pjwstk.edu.pl>
2021-01-19 20:10:36 +0100

    Add extra ficzurs

---
+++ createTable.js
@@ -0,0 +1,106 @@
+$(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'https://coronavirus-19-api.herokuapp.com/countries',
+        success: function (data) {
+            var allowed = ["country", "cases", "deaths", "recovered", "active"];
+
+            var line = "<tr>";
+            $.each(data[0], function (i, row) {
+                if (allowed.includes(i)) {
+                    line += "<th>" + i + "</th>";
+                }
+            })
+            line += "</tr>";
+            $("#table").append(line);
+
+            $.each(data, function (i, row) {
+                if(row["country"] == "World"){
+                    return;
+                }
+                var line = "<tr>";
+                $.each(row, function (i, cell) {
+                    if (allowed.includes(i)) {
+                        if(cell == null){
+                            cell = 0;
+                        }
+                        if (i === "country") {
+                            line += "<td>";
+                            line += "<a href='index.html'>" + cell + "</a>";
+                            //line += "<a href='index.html?c=" + cell + "'>" + cell + "</a>";
+                            line += "</td>";
+                        } else {
+                            line += "<td>" + cell + "</td>";
+                        }
+                    }
+                })
+                line += "</tr>";
+                $("#table").append(line);
+            })
+
+            $("#table tr th:nth-child(1)").on("click", function () {
+                sortTableString("table", 0);
+            });
+            $("#table tr th:nth-child(2)").on("click", function () {
+                sortTableNumber("table", 1);
+            });
+            $("#table tr th:nth-child(3)").on("click", function () {
+                sortTableNumber("table", 2);
+            });
+            $("#table tr th:nth-child(4)").on("click", function () {
+                sortTableNumber("table", 3);
+            });
+            $("#table tr th:nth-child(5)").on("click", function () {
+                sortTableNumber("table", 4);
+            });
+            function sortTableString(name, column) {
+                var table, rows, switching, i, x, y, shouldSwitch;
+                table = document.getElementById(name);
+                switching = true;
+                while (switching) {
+                    switching = false;
+                    rows = table.rows;
+                    for (i = 1; i < (rows.length - 1); i++) {
+                        shouldSwitch = false;
+                        x = rows[i].getElementsByTagName("td")[column];
+                        y = rows[i + 1].getElementsByTagName("td")[column];
+                        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                    if (shouldSwitch) {
+                        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                        switching = true;
+                    }
+                }
+            }
+
+            function sortTableNumber(name, column) {
+                var table, rows, switching, i, x, y, shouldSwitch;
+                table = document.getElementById(name);
+                switching = true;
+                while (switching) {
+                    switching = false;
+                    rows = table.rows;
+                    for (i = 1; i < (rows.length - 1); i++) {
+                        shouldSwitch = false;
+                        x = rows[i + 1].getElementsByTagName("td")[column];
+                        y = rows[i].getElementsByTagName("td")[column];
+                        if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                    if (shouldSwitch) {
+                        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                        switching = true;
+                    }
+                }
+            }
+
+
+        }
+    });
+})
+
--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-$(function (){
-    $.ajax({
-        type: 'GET',
-        url: 'https://coronavirus-19-api.herokuapp.com/countries',
-        success: function(data) {  
-            var allowed = ["country", "cases", "deaths", "recovered", "active"];
-
-            var line = "<tr>";
-            $.each(data[0], function(i, row){
-                if(allowed.includes(i)){
-                    line += "<th>" + i + "</th>";
-                }
-            })
-            line += "</tr>";
-            $("#tbelka").append(line);
-            $.each(data, function(i, row) {
-                var line = "<tr>";
-                $.each(row, function(i, cell){
-                    if(allowed.includes(i)){
-                        line += "<td>" + cell + "</td>";
-                    }
-                })
-                line += "</tr>";
-                $("#tbelka").append(line);
-            })
-
-        }
+$( document ).ready(function() {
+    $("#table tr:first").on("click", function() {
+        console.log("elo");
     });
-})
+});
+
